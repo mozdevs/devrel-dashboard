35297683507aa4f5a35464807cbe3b573334bcee
Dan Callahan <dan.callahan@gmail.com>
2016-03-19 16:00:54 -0500

    Add header to bug table

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 
 import localforage from 'localforage';
 import moment from 'moment';
-import { Table } from 'reactabular';
+import { Table, sortColumn } from 'reactabular';
+import { orderBy } from 'lodash';
 
 /* -------------------------------------------------------------------------- */
 
@@ -90,10 +91,7 @@ const FilterBar = React.createClass({
 });
 
 const BugTable = React.createClass({
-  render: function() {
-    // TODO: Implement overall header
-    // TODO: Implement total / open counts
-
+  getInitialState: function() {
     let columns = [
       { property: 'id', header: 'ID', cell: (id) => (
           <a href={`https://bugzilla.mozilla.org/show_bug.cgi?id=${id}`} target="_blank">{id}</a>
@@ -103,11 +101,15 @@ const BugTable = React.createClass({
       { property: 'resolution', header: 'Resolution' },
       { property: 'product', header: 'Product' },
       { property: 'component', header: 'Component' },
-      { property: 'creation_time', header: 'Age', cell: (date) => ({
-          value: moment(date).fromNow(true),
-          props: { "data-unix": moment(date).unix() }
-      })},
+      { property: 'creation_time', header: 'Age',
+          cell: (date) => moment(date).fromNow(true) }
     ];
+
+    return { sortingColumn: undefined, columns: columns}
+  },
+
+  render: function() {
+    let columns = this.state.columns;
 
     let row = (row) => ({ "data-open": row.is_open });
 
@@ -116,7 +118,28 @@ const BugTable = React.createClass({
       bugs = bugs.filter(bug => bug.is_open);
     }
 
-    return <Table columns={columns} row={row} data={bugs} />;
+    let totalBugs = this.props.bugs.length;
+    let openBugs = this.props.bugs.filter(bug => bug.is_open).length;
+
+    let columnNames = {
+      onClick: (column) => {
+        sortColumn(
+          this.state.columns,
+          column,
+          this.setState.bind(this)
+        );
+      },
+    }
+
+
+    let data = sortColumn.sort(bugs, this.state.sortingColumn, orderBy);
+
+    return (
+      <div>
+        <h1>DevAdvocacy Bugs (Open: {openBugs} / {totalBugs})</h1>
+        <Table columns={columns} row={row} data={data} columnNames={columnNames} />
+      </div>
+    )
   }
 });
 
