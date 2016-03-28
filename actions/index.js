8b4fc5fc094cdd40dc9a3e31649e68bb879b01a5
Dan Callahan <dan.callahan@gmail.com>
2016-03-28 09:52:53 -0500

    Cache Bugzilla responses

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,7 @@
 import fetch from 'isomorphic-fetch';
+import localforage from 'localforage';
+
+const CACHE_VERSION = 1;
 
 // Action Types
 
@@ -22,16 +25,15 @@ export const sortColumn = (column) => ({ type: SORT_COLUMN, column });
 
 export const expireBugs = () => ({ type: EXPIRE_BUGS });
 export const requestBugs = () => ({ type: REQUEST_BUGS });
-export const receiveBugs = (json) => {
+export const receiveBugs = (json, when = Date.now()) => {
   let bugs = json.bugs.reduce((acc, item) => (acc[item.id] = item, acc), {});
 
   return {
     type: RECEIVE_BUGS,
     bugs,
-    receivedAt: Date.now(),
+    receivedAt: when,
   }
 };
-
 
 // Asynchronous Actions
 
@@ -43,22 +45,31 @@ export const fetchBugs = () => {
       'product', 'component', 'creator', 'creation_time', 'whiteboard',
     ];
 
+    let query = bz_base + '?keywords=DevAdvocacy&include_fields=' + bz_fields.join(',');
+
     dispatch(requestBugs());
-    return fetch(bz_base + '?keywords=DevAdvocacy&include_fields=' + bz_fields.join(','))
+    return fetch(query)
            .then(response => response.json())
-           .then(json => dispatch(receiveBugs(json)));
+           .then(json => {
+             let when = Date.now();
+
+             dispatch(receiveBugs(json, when));
+
+             return updateCache(json, when);
+           });
   }
 };
 
 export const fetchBugsIfNeeded = () => {
-  return (dispatch, getState) => {
-    if (shouldFetchBugs(getState())) {
-      return dispatch(fetchBugs());
-    } else {
-      return Promise.resolve();
-    }
-  }
-}
+  return (dispatch, getState) => (
+    loadFromCache()
+    .then(
+      ([json, when]) => dispatch(receiveBugs(json, when)),
+      (e) => { /* Silently ignore cache loading errors */ }
+    )
+    .then(() => shouldFetchBugs(getState()) ? dispatch(fetchBugs()) : undefined)
+  );
+};
 
 // Utilities
 
@@ -69,4 +80,29 @@ const shouldFetchBugs = (state) => {
   let dataIsFresh = lastUpdated && (Date.now() - lastUpdated < day);
 
   return !(isFetching || dataIsFresh);
-}
+};
+
+const loadFromCache = () => {
+  return Promise.all([
+    localforage.getItem('json'),
+    localforage.getItem('when'),
+    localforage.getItem('version')
+  ])
+  .then(([json, when, version]) => {
+    if (version !== CACHE_VERSION) {
+      throw new Error("Cache version mismatch; not using cached data.");
+    } else {
+      return [json, when];
+    }
+  })
+};
+
+const updateCache = (json, when) => {
+  return Promise.all([
+    localforage.setItem('json', json),
+    localforage.setItem('when', when),
+    localforage.setItem('version', CACHE_VERSION)
+  ])
+  .catch(e => localforage.clear())
+  .catch(e => { /* Silently ignore further cache errors */ });
+};
