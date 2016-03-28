e7a31666feec4154f96995ba1817730926d9c2ba
Dan Callahan <dan.callahan@gmail.com>
2016-03-28 16:41:46 -0500

    Allow refreshing data even when the cache is fresh
    Fixes #3

--- components/NetworkStatus.js
+++ components/NetworkStatus.js
@@ -1,11 +1,22 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
+import { fetchBugs } from '../actions'
+
 
 const NetworkStatus = (props) => {
   if (props.fetching) {
-    return <p><em>Fetching data from Bugzilla...</em></p>;
+    return <p>Fetching data from Bugzilla...</p>;
+  } else if (props.lastUpdated === undefined) {
+    return <p>Initializing...</p>
   } else {
-    return <p></p>;
+    return (
+      <p>
+        Last updated {moment(props.lastUpdated).fromNow()}
+        {' '}
+        <a href="#" onClick={props.refresh}>(Refresh)</a>
+      </p>
+    );
   }
 };
 
@@ -15,9 +26,11 @@ NetworkStatus.propTypes = {
 
 const mapStateToProps = (state) => ({
   fetching: state.getIn(['meta', 'isFetching']),
+  lastUpdated: state.getIn(['meta', 'lastUpdated']),
 });
 
 const mapDispatchToProps = (dispatch) => ({
+  refresh: () => dispatch(fetchBugs()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NetworkStatus);
