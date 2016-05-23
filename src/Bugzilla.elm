9a7fa6fe4d93bb09824c85f0d9ddc10ab9c7185e
Dan Callahan <dan.callahan@gmail.com>
2016-05-23 00:51:47 -0500

    WIP Snapshot

--- src/Bugzilla.elm
+++ src/Bugzilla.elm
@@ -1,41 +1,49 @@
 module Bugzilla exposing (Model, Msg, update, view, init)
 
+import Dict exposing (Dict)
 import Html exposing (..)
-import Html.Attributes exposing (..)
+-- import Html.Attributes exposing (..)
 import Http
-import Json.Decode exposing (..)
-import Json.Decode.Pipeline as Pipeline exposing (..)
-import Regex exposing (..)
-import String exposing (..)
+import Json.Decode exposing ((:=), Decoder, at, andThen, int, string, maybe, object3, succeed)
+import Json.Decode.Extra exposing ((|:), dict2)
+import Regex
+import String
 import Task
 
 
 -- MODEL
 
+
 type alias Model =
-  List Bug
+  Dict Int Bug
+
 
 init : (Model, Cmd Msg)
 init =
-  ([], fetch)
+  (Dict.empty, fetch)
+
+
+-- TYPES
+
 
 type alias Bug =
   { id : Int
   , summary : String
-  , status : Status
-  , priority : Priority
   , product : String
   , component : String
+  , state : Maybe State
+  , priority: Maybe Priority
   }
 
-type Status
+
+type State
   = Unconfirmed
   | New
   | Assigned
   | Reopened
   | Resolved Resolution
   | Verified Resolution
-  | UnknownStatus
+
 
 type Resolution
   = Fixed
@@ -44,201 +52,165 @@ type Resolution
   | Duplicate Int
   | WorksForMe
   | Incomplete
-  | UnknownResolution
+
 
 type Priority
   = P1
   | P2
   | P3
   | PX
-  | Untriaged
 
 
 -- UPDATE
 
+
 type Msg
-  = FetchOk (List Bug)
+  = FetchOk (Dict Int Bug)
   | FetchFail Http.Error
+
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    FetchFail _ ->
-      (model, Cmd.none)
-    FetchOk bugs ->
-      (bugs, Cmd.none)
+  (model, Cmd.none)
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div
-    []
-    (List.map viewBug model)
-
-viewBug : Bug -> Html Msg
-viewBug bug =
-  let
-    prodComp =
-      bug.product ++ " :: " ++ bug.component
-  in
-    div
-      [ class "bug"
-      , attribute "data-open" (toString <| bugOpen bug)
-      , attribute "data-status" (bugStatus bug)
-      ]
-      [ div
-          [ class "bug-header" ]
-          [ span [ class "bug-prodcomp", title prodComp ] [ text prodComp ]
-          , bugLink [ class "bug-id" ] bug ("#" ++ (toString bug.id))
-          ]
-      , bugLink [ class "bug-summary" ] bug bug.summary
-      ]
-
-bugLink : List (Html.Attribute Msg) -> Bug -> String -> Html Msg
-bugLink attrs bug label =
-  let
-    url =
-      "https://bugzilla.mozilla.org/show_bug.cgi?id=" ++ (toString bug.id)
-  in
-    a
-      ([ href url
-       , target "_blank"
-       , title (bugStatus bug ++ ": " ++ bug.summary)
-       ] ++ attrs)
-      [ text label ]
-
-bugStatus : Bug -> String
-bugStatus bug =
-  case bug.status of
-    New -> "NEW"
-    Unconfirmed -> "NEW"
-    Assigned -> "ASSIGNED"
-    Reopened -> "REOPENED"
-    Resolved Fixed -> "FIXED"
-    Resolved Invalid -> "INVALID"
-    Resolved WontFix -> "WONTFIX"
-    Resolved WorksForMe -> "WORKSFORME"
-    Resolved Incomplete -> "INCOMPLETE"
-    Resolved (Duplicate id) -> "DUPLICATE"
-    Resolved UnknownResolution -> "(unknown)"
-    Verified Fixed -> "FIXED"
-    Verified Invalid -> "INVALID"
-    Verified WontFix -> "WONTFIX"
-    Verified WorksForMe -> "WORKSFORME"
-    Verified Incomplete -> "INCOMPLETE"
-    Verified (Duplicate id) -> "DUPLICATE"
-    Verified UnknownResolution -> "(unknown)"
-    UnknownStatus -> "(unknown)"
-
-bugOpen : Bug -> Bool
-bugOpen bug =
-  case bug.status of
-    Resolved _ -> False
-    _ -> True
-
-urlForId : Int -> String
-urlForId id =
-  "https://bugzilla.mozilla.org/show_bug.cgi?id=" ++ (toString id)
+  text "Hello, world."
 
 
 -- HTTP
+
 
 fetch : Cmd Msg
 fetch =
   let
     url =
-      -- https://bugzilla.mozilla.org/rest/bug?keywords=DevAdvocacy&include_fields=id,summary,status,resolution,is_open,dupe_of,product,component,creator,creation_time,whiteboard
-      "http://localhost:3000/db"
+      Http.url
+        -- "https://bugzilla.mozilla.org/rest/bug"
+        "http://localhost:3000/db"
+        [ (,) "keywords" "DevAdvocacy"
+        , (,)
+            "include_fields"
+            (String.join ","
+              [ "id"
+              , "summary"
+              , "status"
+              , "resolution"
+              , "dupe_of"
+              , "product"
+              , "component"
+              , "whiteboard"
+              ])
+        ]
   in
-    Task.perform FetchFail FetchOk (Http.get decodeBugs url)
-
-decodeBugs : Decoder (List Bug)
-decodeBugs =
-  at ["bugs"] (Json.Decode.list decodeBug)
-
-decodeBug : Decoder Bug
-decodeBug =
-  Pipeline.decode Bug
-    |> Pipeline.required "id" int
-    |> Pipeline.required "summary" string
-    |> Pipeline.custom statusDecoder
-    |> Pipeline.custom priorityDecoder
-    |> Pipeline.required "product" string
-    |> Pipeline.required "component" string
-    -- Other fields of interest:
-    -- open, created, creator
+    Task.perform FetchFail FetchOk (Http.get bugDecoder url)
 
 
-statusDecoder : Decoder Status
-statusDecoder =
-  object3 (,,)
-    ("status" := string)
-    ("resolution" := string)
-    (maybe ("dupe_of" := int))
-  `andThen`
-  statusInfo
+-- JSON
 
-statusInfo : (String, String, Maybe Int) -> Decoder Status
-statusInfo (status, resolution, dupe) =
-  let result =
-    case status of
-      "NEW" -> New
-      "UNCONFIRMED" -> Unconfirmed
-      "ASSIGNED" -> Assigned
-      "RESOLVED" -> case resolution of
-        "FIXED" -> Resolved Fixed
-        "INVALID" -> Resolved Invalid
-        "WONTFIX" -> Resolved WontFix
-        "DUPLICATE" -> case dupe of
-          Just id -> Resolved (Duplicate id)
-          Nothing -> Resolved UnknownResolution
-        "WORKSFORME" -> Resolved WorksForMe
-        "INCOMPLETE" -> Resolved Incomplete
-        _ -> Resolved UnknownResolution
-      "VERIFIED" -> case resolution of
-        "FIXED" -> Verified Fixed
-        "INVALID" -> Verified Invalid
-        "WONTFIX" -> Verified WontFix
-        "DUPLICATE" -> case dupe of
-          Just id -> Verified (Duplicate id)
-          Nothing -> Verified UnknownResolution
-        "WORKSFORME" -> Verified WorksForMe
-        "INCOMPLETE" -> Verified Incomplete
-        _ -> Verified UnknownResolution
-      "REOPENED" -> Reopened
-      _ -> UnknownStatus
-  in
-    succeed result
 
-priorityDecoder : Decoder Priority
-priorityDecoder =
-  ("whiteboard" := string) `andThen` prioFromWhiteboard
+bugDecoder : Decoder (Dict Int Bug)
+bugDecoder =
+  at ["bugs"] (dict2 decId decBug)
 
-prioFromWhiteboard : String -> Decoder Priority
-prioFromWhiteboard wb =
+
+decId : Decoder Int
+decId =
+  "id" := int
+
+
+decBug : Decoder Bug
+decBug =
+  succeed Bug
+    |: ("id" := int)
+    |: ("summary" := string)
+    |: ("product" := string)
+    |: ("component" := string)
+    |: andThen -- "state"
+         (object3 (,,)
+           ("status" := string)
+           ("resolution" := string)
+           ("dupe_of" := maybe int))
+         decState
+    |: andThen -- "priority"
+         ("whiteboard" := string)
+         decPrio
+
+
+decState : (String, String, Maybe Int) -> Decoder (Maybe State)
+decState (status, resolution, dupeOf) =
   let
-    wb' =
-      toLower wb
-    re =
-      Regex.regex "\\[devrel:p(.)\\]"
-    matches =
-      Regex.find (Regex.AtMost 1) re wb'
-    p =
-      case matches of
-        [] -> 
-          Untriaged
-        x :: _ ->
-          case x.submatches of
-            [] ->
-              Untriaged
-            y :: _ ->
-              case y of 
-                Just "1" -> P1
-                Just "2" -> P2
-                Just "3" -> P3
-                Just "x" -> PX
-                _ -> Untriaged
+    resolution' =
+      case resolution of
+        "FIXED" ->
+          Just Fixed
+
+        "INVALID" ->
+          Just Invalid
+
+        "WONTFIX" ->
+          Just WontFix
+
+        "DUPLICATE" ->
+          Maybe.map (\id -> Duplicate id) dupeOf
+
+        "WORKSFORME" ->
+          Just WorksForMe
+
+        "INCOMPLETE" ->
+          Just Incomplete
+
+        _ ->
+          Nothing
+
+    state =
+      case status of
+        "UNCONFIRMED" ->
+          Just Unconfirmed
+
+        "NEW" ->
+          Just New
+
+        "ASSIGNED" ->
+          Just Assigned
+
+        "REOPENED" ->
+          Just Reopened
+
+        "RESOLVED" ->
+          Maybe.map (\x -> Resolved x) resolution'
+
+        "VERIFIED" ->
+          Maybe.map (\x -> Verified x) resolution'
+
+        _ ->
+          Nothing
   in
-    succeed p
+    succeed state
+
+
+decPrio : String -> Decoder (Maybe Priority)
+decPrio whiteboard =
+  let
+    pattern =
+      Regex.regex "\\[devrel:p(.)\\]"
+
+    matches =
+      Regex.find (Regex.AtMost 1) pattern (String.toLower whiteboard)
+
+    submatches =
+      List.map (\x -> x.submatches) matches
+
+    priority =
+      case submatches of
+        [Just "1" :: _] -> Just P1
+        [Just "2" :: _] -> Just P2
+        [Just "3" :: _] -> Just P3
+        [Just "x" :: _] -> Just PX
+        _ -> Nothing
+  in
+    succeed priority
